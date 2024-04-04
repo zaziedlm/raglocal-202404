411334964f61a03bf75458fa22bb5da2b5808958
Shuichi Kataoka <shu01k9@gmail.com>
2024-04-04 17:18:55 +0900

    ローカルPC整備、実行可能版リリース

---
+++ README.md
@@ -0,0 +1,17 @@
+# LlamaIndex v0.10.26
+A LlamaIndex RAG sample, local LLM & embeded LLM
+for OpenAI Python API library 1.X
+
+# first! prepare env, install Build Tools for Visual Studio 2022
+# https://visualstudio.microsoft.com/ja/downloads/
+
+# poetry require modules. 
+poetry install --no-root, then set require modules from pyproject.toml
+
+# set configuration-env,  input-data(PDF), LLM model
+mkdir data/     (and set a PDF file, for RAG Search)
+git lfs clone  https://huggingface.co/mmnga/ELYZA-japanese-Llama-2-7b-instruct-gguf/ --include "ELYZA-japanese-Llama-2-7b-instruct-q8_0.gguf"
+copy ELYZA-japanese-Llama-2-7b-instruct-q8_0.gguf to %Appdata%/local/llama_index/models/
+
+# run application command. 
+python .\raglocal.py  
--- raglocal.py
+++ raglocal.py
@@ -17,3 +17,69 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, force=True)
 # Read document from data-folder.
 document = SimpleDirectoryReader(os.path.join(os.path.dirname(__file__), "data")).load_data()
 
+from llama_index.llms.llama_cpp import LlamaCPP
+
+#model_path = f"models/ELYZA-japanese-Llama-2-7b-fast-instruct-gguf/ELYZA-japanese-Llama-2-7b-instruct-q8_0.gguf"
+model_path = f"ELYZA-japanese-Llama-2-7b-instruct-q8_0.gguf"
+llm = LlamaCPP(
+    model_url=model_path,
+    temperature=0.1,
+    model_kwargs={"n_ctx": 4096, "n_gpu_layer": 32},
+)
+EMBEDDING_DEVICE = "cpu"
+
+# embed_model_name = "sentence-transformers/all-mpnet-base-v2"
+embed_model_name = "intfloat/multilingual-e5-large"
+cache_folder = "./sentence_transformers"
+
+embed_model = HuggingFaceEmbedding(
+    model_name=embed_model_name,
+    cache_folder=cache_folder,
+    device=EMBEDDING_DEVICE,
+)
+
+# ServiceContext was deprecated, globally in the Settings
+from llama_index.core import Settings
+from llama_index.core.node_parser import SentenceSplitter
+
+Settings.llm = llm
+Settings.embed_model = embed_model
+Settings.node_parser = SentenceSplitter(chunk_size=500, chunk_overlap=20)
+Settings.num_output = 256
+Settings.context_window = 3900
+
+index = VectorStoreIndex.from_documents(
+    document,
+)
+
+# 質問
+temp = """
+[INST]
+<<SYS>>
+以下の「コンテキスト情報」を元に「質問」に回答してください。
+なお、コンテキスト情報に無い情報は回答に含めないでください。
+また、コンテキスト情報から回答が導けない場合は「分かりません」と回答してください。
+<</SYS>>
+# コンテキスト情報
+---------------------
+{context_str}
+---------------------
+
+# 質問
+{query_str}
+
+[/INST]
+"""
+
+query_engine = index.as_query_engine(
+    similarity_top_k=5,
+    text_qa_template=PromptTemplate(temp),
+)
+
+while True:
+    req_msg = input("\n## Question: ")
+    if req_msg == "":
+        continue
+    res_msg = query_engine.query(req_msg)
+    res_msg.source_nodes[0].text
+    print("\n## Answer: \n", str(res_msg).strip())
